cede2b1cacddb0d433c9b14aa68ef92cbf4cc43b
Krizel Rika <krizelcodes@gmail.com>
2025-11-05 20:54:59 +0800

    initial LinkedList class

--- LinkedList.js
+++ LinkedList.js
@@ -5,3 +5,117 @@ class Node {
     this.nextNode = nextNode;
   }
 }
+
+// LinkedList class managing the list
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+  }
+
+  // Adds a new node containing value to the end of the list
+  append(value) {
+    const newNode = new Node(value);
+    if (this.headNode === null) {
+      this.headNode = newNode;
+      return;
+    }
+    let current = this.headNode;
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    current.nextNode = newNode;
+  }
+
+  // Adds a new node containing value to the start of the list
+  prepend(value) {
+    const newNode = new Node(value, this.headNode);
+    this.headNode = newNode;
+  }
+
+  // Returns the total number of nodes in the list
+  size() {
+    let count = 0;
+    let current = this.headNode;
+    while (current) {
+      count++;
+      current = current.nextNode;
+    }
+    return count;
+  }
+
+  // Returns the first node in the list
+  head() {
+    return this.headNode;
+  }
+
+  // Returns the last node in the list
+  tail() {
+    if (!this.headNode) return null;
+    let current = this.headNode;
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    return current;
+  }
+
+  // Returns the node at the given index
+  at(index) {
+    if (index < 0) return null;
+    let current = this.headNode;
+    let count = 0;
+    while (current !== null) {
+      if (count === index) return current;
+      count++;
+      current = current.nextNode;
+    }
+    return null; // index out of range
+  }
+
+  // Removes the last element from the list
+  pop() {
+    if (!this.headNode) return null;
+    if (!this.headNode.nextNode) {
+      this.headNode = null;
+      return;
+    }
+    let current = this.headNode;
+    while (current.nextNode.nextNode !== null) {
+      current = current.nextNode;
+    }
+    current.nextNode = null;
+  }
+
+  // Returns true if the passed in value is in the list
+  contains(value) {
+    let current = this.headNode;
+    while (current !== null) {
+      if (current.value === value) return true;
+      current = current.nextNode;
+    }
+    return false;
+  }
+
+  // Returns the index of the node containing value, or null if not found
+  find(value) {
+    let index = 0;
+    let current = this.headNode;
+    while (current !== null) {
+      if (current.value === value) return index;
+      index++;
+      current = current.nextNode;
+    }
+    return null;
+  }
+
+  // Represents your LinkedList objects as strings
+  toString() {
+    let result = "";
+    let current = this.headNode;
+    while (current !== null) {
+      result += `( ${current.value} ) -> `;
+      current = current.nextNode;
+    }
+    return result + "null";
+  }
+
+}
